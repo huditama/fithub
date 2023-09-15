6561b16081106c0c28788ab6b1ce4d321052d75d
Reyhan Huditama <reyhan21098@gmail.com>
2023-09-15 19:00:30 +0700

    feat: handle error get classes API

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,18 +1,22 @@
 import { Endpoints } from '@/api/endpoints';
 
 export const getClasses = async (dateFrom: string, dateTo: string) => {
-  const res = await fetch(Endpoints.GET_SCHEDULES, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      dateFrom,
-      dateTo,
-      selectedClub: 'FIT HUB ARTERI PONDOK INDAH',
-      selectedCategory: 'ALL',
-    }),
-  });
+  try {
+    const res = await fetch(Endpoints.GET_SCHEDULES, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        dateFrom,
+        dateTo,
+        selectedClub: 'FIT HUB ARTERI PONDOK INDAH',
+        selectedCategory: 'ALL',
+      }),
+    });
 
-  return res.json();
+    return await res.json();
+  } catch (err) {
+    return err;
+  }
 };
